7b433b9bc2450106143667ef0d2bb35c9077fd0b
Paulina Kolonczyk <paulinakolonczyk@interia.pl>
2021-09-12 13:07:14 +0200

    Add user joined

--- client/app.js
+++ client/app.js
@@ -2,6 +2,8 @@
 
 const socket = io();
 socket.on('message', ({author, content}) => addMessage(author, content));
+socket.on('newUser', ({ author, content }) => addOrRemoveUserMessage(author, content));
+socket.on('userHasLeft', ({ author, content }) => addOrRemoveUserMessage(author, content));
 
 const references = {
   loginForm: document.getElementById('welcome-form'),
@@ -20,6 +22,7 @@ const login = function(event) {
     userName = references.userNameInput.value;
     references.messagesSection.classList.add('show');
     references.loginForm.classList.remove('show');
+    socket.emit('login', {name: userName, id: socket.id });
   } else window.alert('Write your login!');
 };
 
@@ -34,6 +37,21 @@ function addMessage(author, content) {
     <h3 class="message__author">${userName === author ? 'You' : author }</h3>
     <div class="message__content">
       ${content}
+    </div>
+  `;
+  references.messagesList.appendChild(message);
+}
+
+function addOrRemoveUserMessage(author, content) {
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+  message.classList.add('message--self');
+  message.classList.add('message--changeUser');
+  message.innerHTML = `
+    <h3 class="message__author">${ author }</h3>
+    <div class="message__content">
+      ${ content }
     </div>
   `;
   references.messagesList.appendChild(message);
--- server.js
+++ server.js
@@ -1,30 +1,44 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const socket = require('socket.io');
-const messages = [];
 const app = express();
-app.use(express.static(path.join(__dirname, "/client")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/client/index.html"));
+let messages = [];
+let users = [];
+
+app.use(express.static(path.join(__dirname, '/client')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/index.html'));
 });
 
-app.use((req, res) => {
-  res.status(404).send("404 not found...");
+app.use((req,res) => {
+  return res.status(404).json({
+    message: 'Not found...'
+  });
 });
+
 const server = app.listen(8000, () => {
-  console.log("Server is running on port: 8000");
+  console.log('Server is running on port 8000: http://localhost:8000');
 });
 
 const io = socket(server);
 
 io.on('connection', (socket) => {
   console.log('New client! Its id – ' + socket.id);
+  socket.on('login', (user) => {
+    users.push(user);
+    socket.broadcast.emit('newUser', {author: 'Chat Boy', content: '' + user.name + ' has joined the conversation!'});
+  });
   socket.on('message', (message) => {
     console.log('Oh, I\'ve got something from ' + socket.id);
     messages.push(message);
     socket.broadcast.emit('message', message);
   });
-  socket.on('disconnect', () => { console.log('Oh, user ' + socket.id + ' has left :(')});
+  socket.on('disconnect', () => {
+    const name = (users.filter(user => user.id === socket.id))[0].name;
+    users = users.filter(user => user.id !== socket.id);
+    socket.broadcast.emit('userHasLeft', {author: 'Chat Boy', content: '' + name + ' has left the conversation... :('} )
+  });
   console.log('I\'ve added a listener on message and disconnect events \n');
-}); 
+});
